354595ddbf9deb7f057f31e68050132d2034e97a
秋雨落 <i@rain.cx>
2020-07-01 13:19:42 +0800

    Add: 微小的工作
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemPepperGreenShreddedMeat.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemPepperGreenShreddedMeat.java
@@ -3,27 +3,17 @@ package cx.rain.mc.forgemod.culturecraft.item.automatic;
 import cx.rain.mc.forgemod.culturecraft.api.annotation.ModItem;
 import cx.rain.mc.forgemod.culturecraft.group.Groups;
 import cx.rain.mc.forgemod.culturecraft.registry.RegistryItem;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
-import java.util.List;
-
-@ModItem(name = "greenpepper_shreddedmeat")
-public class Itemgreenpeppershreddedmeat extends Item {
+@ModItem(name = "green_pepper_shredded_meat")
+public class ItemPepperGreenShreddedMeat extends Item {
     private static Food food = new Food.Builder()
             .hunger(7)
             .saturation(10)
             .build();
 
-    public Itemgreenpeppershreddedmeat() {
+    public ItemPepperGreenShreddedMeat() {
         super(new Properties()
                 .group(Groups.FOODS)
                 .food(food)
@@ -31,9 +21,5 @@ public class Itemgreenpeppershreddedmeat extends Item {
                 .setNoRepair()
                 .containerItem(RegistryItem.ITEMS.get("plate"))
         );
-
-
     }
-
-
 }
