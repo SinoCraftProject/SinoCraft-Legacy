73fa755b1846dc532d494eb4c1f4ac5dd401acec
SQwatermark <sqwatermark@outlook.com>
2020-07-04 10:47:59 +0800

    Fix: 鸣鸿刀的id和模型 gitignore多余的设置

--- src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemChirpingWildGoose.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemChirpingWildGoose.java
@@ -4,13 +4,12 @@ import cx.rain.mc.forgemod.culturecraft.api.annotation.ModItem;
 import cx.rain.mc.forgemod.culturecraft.group.Groups;
 import net.minecraft.item.*;
 
-@ModItem(name = "chirping_wildoose")
-public class ItemChirpingwildgoose extends SwordItem {
-
-    public ItemChirpingwildgoose() {
-        super(ItemTier.DIAMOND, 9, -2.4F, new ItemKnife.Properties()
+@ModItem(name = "chirping_wildgoose")
+public class ItemChirpingWildGoose extends SwordItem {
+    public ItemChirpingWildGoose() {
+        super(ItemTier.DIAMOND, 9, -2.4F, new Item.Properties()
                 .group(Groups.COMBAT)
                 .setNoRepair()
                 .maxStackSize(1));
-
-}}
+    }
+}
