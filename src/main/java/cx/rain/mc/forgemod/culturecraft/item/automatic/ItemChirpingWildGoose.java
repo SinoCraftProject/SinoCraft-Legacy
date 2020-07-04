af8ff89b79b8bbe2e8ad58e1e59e701120b30fda
SQwatermark <sqwatermark@outlook.com>
2020-07-04 11:37:01 +0800

    Delete: 删除多余的模型文件

--- src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemChirpingWildGoose.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemChirpingWildGoose.java
@@ -2,7 +2,9 @@ package cx.rain.mc.forgemod.culturecraft.item.automatic;
 
 import cx.rain.mc.forgemod.culturecraft.api.annotation.ModItem;
 import cx.rain.mc.forgemod.culturecraft.group.Groups;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
 
 @ModItem(name = "chirping_wildgoose")
 public class ItemChirpingWildGoose extends SwordItem {
