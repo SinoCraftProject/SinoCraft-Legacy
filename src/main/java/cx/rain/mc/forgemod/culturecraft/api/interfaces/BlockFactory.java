8cbf88008aa8da18a1125f5b1a95eac64ebf49d9
theflysong <song_of_the_fly@163.com>
2020-07-06 00:50:55 +0800

    Add: 一些关于工厂方法的接口 Modify: ModBlock.java & commit rule.java TODO: 修改自动注册，使其可以接受一个工厂类的Class并通过该工厂类获取方块实例

--- src/main/java/cx/rain/mc/forgemod/culturecraft/api/interfaces/BlockFactory.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/api/interfaces/BlockFactory.java
@@ -1,6 +1,7 @@
 package cx.rain.mc.forgemod.culturecraft.api.interfaces;
 
 import net.minecraft.block.Block;
+import java.lang.reflect.InvocationTargetException;
 
 public class BlockFactory implements IBlockFactory{
     @Override
--- src/main/java/cx/rain/mc/forgemod/culturecraft/api/interfaces/IFactory.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/api/interfaces/IFactory.java
@@ -1,5 +1,7 @@
 package cx.rain.mc.forgemod.culturecraft.api.interfaces;
 
+import java.lang.reflect.InvocationTargetException;
+
 public interface IFactory<T,R>{
-    T get(R type);
+    T get(R type) throws NoSuchMethodException,IllegalAccessException,InstantiationException,InvocationTargetException;
 }
