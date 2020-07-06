71f4205fd6dd8172963eb5a90f8e62e8633a0c43
秋雨落 <i@rain.cx>
2020-07-06 12:40:07 +0800

    Change: 依然是一点点微小的工作
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemKnife.java
+++ src/main/java/cx/rain/mc/forgemod/culturecraft/item/automatic/ItemKnife.java
@@ -2,12 +2,14 @@ package cx.rain.mc.forgemod.culturecraft.item.automatic;
 
 import cx.rain.mc.forgemod.culturecraft.api.annotation.ModItem;
 import cx.rain.mc.forgemod.culturecraft.group.Groups;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
+
 @ModItem(name = "knife")
-public class ItemKnife extends Item {
+public class ItemKnife extends SwordItem {
     public ItemKnife(){
-        super(new Properties()
-        .maxStackSize(1)
-        .group(Groups.MISC));
+        super(ItemTier.IRON, 1, -1F, new Properties()
+                .maxStackSize(1)
+                .group(Groups.MISC));
     }
 }
