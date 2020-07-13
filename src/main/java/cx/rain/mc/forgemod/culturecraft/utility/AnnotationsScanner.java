7bc04e8597b374c50c42d6663d1c3908d741f9c3
秋雨落 <i@rain.cx>
2020-07-13 17:47:12 +0800

    Fix: Compile error
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/forgemod/culturecraft/utility/AnnotationsScanner.java
+++
@@ -1,9 +0,0 @@
-package cx.rain.mc.forgemod.culturecraft.utility;
-
-import org.objectweb.asm.ClassVisitor;
-
-public class AnnotationsScanner extends ClassVisitor {
-    public AnnotationsScanner(int api) {
-        super(api);
-    }
-}
