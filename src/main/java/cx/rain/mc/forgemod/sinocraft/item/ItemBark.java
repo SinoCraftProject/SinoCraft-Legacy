bdc719ef1339377b763abfb68cc34221645d6518
theflysong <song_of_the_fly@163.com>
2020-07-18 19:14:57 +0800

    Fix: Compilie Error

--- src/main/java/cx/rain/mc/forgemod/sinocraft/item/ItemBark.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/item/ItemBark.java
@@ -4,10 +4,10 @@ import cx.rain.mc.forgemod.sinocraft.group.Groups;
 import cx.rain.mc.forgemod.sinocraft.enumerate.LogType;
 import net.minecraft.item.Item;
 
-public class ItemFood extends Item {
+public class ItemBark extends Item {
 	private LogType type;
 
-    public ItemFood(LogType typeIn) {
+    public ItemBark(LogType typeIn) {
         super(new Item.Properties().group(Groups.MISC));
         this.type=typeIn;
     }
