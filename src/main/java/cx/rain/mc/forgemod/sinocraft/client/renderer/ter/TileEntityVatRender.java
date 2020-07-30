80f155fe86b8dbfec2be7da6e1a164984a2d5785
theflysong <song_of_the_fly@163.com>
2020-07-30 09:21:45 +0800

    造纸术25%
    Doing:造纸术、木桶的TER
    Fix: Compiler Error
    Add: Util:Vec2 Vec3 Vec4(for fix compiler error)

--- src/main/java/cx/rain/mc/forgemod/sinocraft/client/renderer/ter/TileEntityVatRender.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/client/renderer/ter/TileEntityVatRender.java
@@ -2,8 +2,9 @@ package cx.rain.mc.forgemod.sinocraft.client.renderer.ter;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
-import com.sun.javafx.geom.Vec3f;
-import com.sun.javafx.geom.Vec4f;
+import cx.rain.mc.forgemod.sinocraft.api.util.math.Vec2;
+import cx.rain.mc.forgemod.sinocraft.api.util.math.Vec3;
+import cx.rain.mc.forgemod.sinocraft.api.util.math.Vec4;
 import cx.rain.mc.forgemod.sinocraft.tileentity.TileEntityVat;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
@@ -15,9 +16,6 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.inventory.container.PlayerContainer;
-import net.minecraft.util.math.Vec2f;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
@@ -26,11 +24,11 @@ public class TileEntityVatRender extends TileEntityRenderer<TileEntityVat> {
         super(dispatcher);
     }
 
-    public static void add(IVertexBuilder builder, MatrixStack matrixStack, Vec3f pos, Vec2f uv){
-        add(builder,matrixStack,pos,new Vec4f(1.0f,1.0f,1.0f,1.0f),uv);
+    public static void add(IVertexBuilder builder, MatrixStack matrixStack, Vec3<Float> pos, Vec2<Float> uv){
+        add(builder,matrixStack,pos,new Vec4(1.0f,1.0f,1.0f,1.0f),uv);
     }
 
-    public static void add(IVertexBuilder builder, MatrixStack matrixStack, Vec3f pos,Vec4f color, Vec2f uv){
+    public static void add(IVertexBuilder builder, MatrixStack matrixStack, Vec3<Float> pos, Vec4<Float> color, Vec2<Float> uv){
         builder.pos(matrixStack.getLast().getMatrix(), pos.x, pos.y, pos.z)
                 .color(color.x, color.y, color.z, color.w)
                 .tex(uv.x, uv.y)
@@ -39,63 +37,65 @@ public class TileEntityVatRender extends TileEntityRenderer<TileEntityVat> {
                 .endVertex();
     }
 
-    public static void addSquare(IVertexBuilder builder,MatrixStack matrixStack,Vec3f pos1,Vec2f uv1,Vec3f pos2,Vec2f uv2,Vec3f pos3,Vec2f uv3,Vec3f pos4,Vec2f uv4){
+    public static void addSquare(IVertexBuilder builder,MatrixStack matrixStack,Vec3<Float> pos1,Vec2<Float> uv1,Vec3<Float> pos2,Vec2<Float> uv2,Vec3<Float> pos3
+            ,Vec2<Float> uv3,Vec3<Float> pos4,Vec2<Float> uv4){
         add(builder,matrixStack,pos1,uv1);
         add(builder,matrixStack,pos2,uv2);
         add(builder,matrixStack,pos3,uv3);
         add(builder,matrixStack,pos4,uv4);
     }
 
-    public static void addSquare(IVertexBuilder builder, MatrixStack matrixStack, Vec3f pos1, Vec3f pos2, Vec3f pos3, Vec3f pos4, Vec4f uv){
+    public static void addSquare(IVertexBuilder builder, MatrixStack matrixStack, Vec3<Float> pos1, Vec3<Float> pos2, Vec3<Float> pos3, Vec3<Float> pos4, Vec4<Float> uv){
         addSquare(builder,matrixStack,
-                pos1,new Vec2f(uv.x,uv.z),
-                pos2,new Vec2f(uv.y,uv.z),
-                pos3,new Vec2f(uv.x,uv.w),
-                pos4,new Vec2f(uv.y,uv.w));
+                pos1,new Vec2(uv.x,uv.z),
+                pos2,new Vec2(uv.y,uv.z),
+                pos3,new Vec2(uv.x,uv.w),
+                pos4,new Vec2(uv.y,uv.w));
     }
 
-    public static void addSquare(IVertexBuilder builder,MatrixStack matrixStack,
-                                 Vec3f pos1,Vec2f uv1,Vec4f color1,
-                                 Vec3f pos2,Vec2f uv2,Vec4f color2,
-                                 Vec3f pos3,Vec2f uv3,Vec4f color3,
-                                 Vec3f pos4,Vec2f uv4,Vec4f color4){
+    public static void addSquare(IVertexBuilder builder, MatrixStack matrixStack,
+                                 Vec3<Float> pos1, Vec2<Float> uv1, Vec4<Float> color1,
+                                 Vec3<Float> pos2, Vec2<Float> uv2, Vec4<Float> color2,
+                                 Vec3<Float> pos3, Vec2<Float> uv3, Vec4<Float> color3,
+                                 Vec3<Float> pos4, Vec2<Float> uv4, Vec4<Float> color4){
         add(builder,matrixStack,pos1,color1,uv1);
         add(builder,matrixStack,pos2,color2,uv2);
         add(builder,matrixStack,pos3,color3,uv3);
         add(builder,matrixStack,pos4,color4,uv4);
     }
 
-    public static void addSquare(IVertexBuilder builder, MatrixStack matrixStack, Vec3f pos1, Vec3f pos2, Vec3f pos3, Vec3f pos4, Vec4f uv,Vec4f color){
+    public static void addSquare(IVertexBuilder builder, MatrixStack matrixStack, Vec3<Float> pos1, Vec3<Float> pos2, Vec3<Float> pos3,
+                                 Vec3<Float> pos4, Vec4<Float> uv,Vec4<Float> color){
         addSquare(builder,matrixStack,
-                pos1,new Vec2f(uv.x,uv.z),color,
-                pos2,new Vec2f(uv.y,uv.z),color,
-                pos3,new Vec2f(uv.x,uv.w),color,
-                pos4,new Vec2f(uv.y,uv.w),color);
+                pos1,new Vec2(uv.x,uv.z),color,
+                pos2,new Vec2(uv.y,uv.z),color,
+                pos3,new Vec2(uv.x,uv.w),color,
+                pos4,new Vec2(uv.y,uv.w),color);
     }
 
-    public static Vec4f RGBAToGLColor(Vec4f rgba){
+    public static Vec4<Float> RGBAToGLColor(Vec4<Float> rgba){
         rgba.x/=256;
         rgba.y/=256;
         rgba.z/=256;
         rgba.w/=100;
         return rgba;
     }
-    public static Vec4f GLColorToRGBA(Vec4f glColor){
+    public static Vec4<Float> GLColorToRGBA(Vec4<Float> glColor){
         glColor.x*=256;
         glColor.y*=256;
         glColor.z*=256;
         glColor.w*=100;
         return glColor;
     }
-    public static Vec4f NumberToRGBA(int color){
-        int a=color>>>24;
-        int r=(color&0xff0000)>>16;
-        int g=(color&0xff00)>>8;
-        int b=color&0xff;
-        return new Vec4f(r,g,b,a);
+    public static Vec4<Float> NumberToRGBA(int color){
+        float a=color>>>24;
+        float r=(color&0xff0000)>>16;
+        float g=(color&0xff00)>>8;
+        float b=color&0xff;
+        return new Vec4(r,g,b,a);
     }
-    public static Vec4f NumberToGLColor(int color){
-        Vec4f rgba = NumberToRGBA(color);
+    public static Vec4<Float> NumberToGLColor(int color){
+        Vec4<Float> rgba = NumberToRGBA(color);
         return RGBAToGLColor(rgba);
     }
 
@@ -115,9 +115,9 @@ public class TileEntityVatRender extends TileEntityRenderer<TileEntityVat> {
                 matrixStack.scale(0.75f,1.0f,0.75f);
                 matrixStack.translate(0.18,0.00001,0.18);
                 addSquare(vertex,matrixStack,
-                        new Vec3f(0,1,1),new Vec3f(1,1,1),
-                        new Vec3f(1,1,0),new Vec3f(0,1,0),
-                        new Vec4f(sprite.getMinU(),sprite.getMaxU(),sprite.getMinV(),sprite.getMaxV()),
+                        new Vec3(0.0f,1.0f,1.0f),new Vec3(1.0f,1.0f,1.0f),
+                        new Vec3(1.0f,1.0f,0.0f),new Vec3(0.0f,1.0f,0.0f),
+                        new Vec4(sprite.getMinU(),sprite.getMaxU(),sprite.getMinV(),sprite.getMaxV()),
                         NumberToGLColor(fluid.getAttributes().getColor())
                 );
                 matrixStack.pop();
