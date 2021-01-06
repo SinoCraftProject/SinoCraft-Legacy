b6ff0efd0e076aba35c80ce67f5bb7ae2a4153ae
秋雨落 <i@rain.cx>
2021-01-06 15:23:45 +0800

    :sparkles: 终于修好了石头的生成
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/forgemod/sinocraft/world/ModFeatures.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/world/ModFeatures.java
@@ -1,37 +1,30 @@
 package cx.rain.mc.forgemod.sinocraft.world;
 
 import cx.rain.mc.forgemod.sinocraft.SinoCraft;
-import cx.rain.mc.forgemod.sinocraft.block.ModBlocks;
-import cx.rain.mc.forgemod.sinocraft.world.config.FeatureConfigs;
+import cx.rain.mc.forgemod.sinocraft.world.config.Features;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.OreFeature;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Objects;
 
-@Mod.EventBusSubscriber(modid = SinoCraft.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Features {
-    public static final ConfiguredFeature<?, ?> WHITE_MARBLE = new ConfiguredFeature<>(new OreFeature(OreFeatureConfig.CODEC), FeatureConfigs.WHITE_MARBLE);
-    public static final ConfiguredFeature<?, ?> BLACK_MARBLE = new ConfiguredFeature<>(new OreFeature(OreFeatureConfig.CODEC), FeatureConfigs.BLACK_MARBLE);
-    public static final ConfiguredFeature<?, ?> RED_MARBLE = new ConfiguredFeature<>(new OreFeature(OreFeatureConfig.CODEC), FeatureConfigs.RED_MARBLE);
-
+@Mod.EventBusSubscriber(modid = SinoCraft.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModFeatures {
     @SubscribeEvent
     public static void onBiomeLoading(BiomeLoadingEvent event) {
         BiomeGenerationSettingsBuilder gen = event.getGeneration();
         RegistryKey<Biome> biome = RegistryKey.getOrCreateKey(ForgeRegistries.Keys.BIOMES,
                 Objects.requireNonNull(event.getName(), "Who registered null name biome, naming criticism!"));
 
+        System.out.println("0");
         if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
+            System.out.println("1");
             addFeatureWorld(gen);
         }
 
@@ -41,11 +34,11 @@ public class Features {
     }
 
     private static void addFeatureWorld(BiomeGenerationSettingsBuilder generation) {
-        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, WHITE_MARBLE);
-        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, BLACK_MARBLE);
+        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.WHITE_MARBLE);
+        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.BLACK_MARBLE);
     }
 
     private static void addFeatureNether(BiomeGenerationSettingsBuilder generation) {
-        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, RED_MARBLE);
+        generation.withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Features.RED_MARBLE);
     }
 }
--- src/main/java/cx/rain/mc/forgemod/sinocraft/world/config/FeatureConfigs.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/world/config/FeatureConfigs.java
@@ -4,9 +4,9 @@ import cx.rain.mc.forgemod.sinocraft.block.ModBlocks;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class FeatureConfigs {
-    public static final OreFeatureConfig WHITE_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.WHITE_MARBLE.get().getDefaultState(), 16);
-    public static final OreFeatureConfig BLACK_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.BLACK_MARBLE.get().getDefaultState(), 16);
-    public static final OreFeatureConfig RED_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, ModBlocks.RED_MARBLE.get().getDefaultState(), 16);
+    public static final OreFeatureConfig WHITE_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.WHITE_MARBLE.get().getDefaultState(), 32);
+    public static final OreFeatureConfig BLACK_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.BLACK_MARBLE.get().getDefaultState(), 32);
+    public static final OreFeatureConfig RED_MARBLE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, ModBlocks.RED_MARBLE.get().getDefaultState(), 32);
 
-    
+
 }
