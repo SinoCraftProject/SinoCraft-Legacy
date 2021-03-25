5507b0fe17edee998d6d60ee3f956ad075cdccf8
theflysong <song_of_the_fly@163.com>
2021-03-25 22:40:16 +0800

    Update Tutorial

--- src/main/java/cx/rain/mc/forgemod/sinocraft/gui/book/component/TutorialComponent.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/gui/book/component/TutorialComponent.java
@@ -10,6 +10,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public abstract class TutorialComponent implements IRenderable {
     protected GuiTutorialBook.Page page;
     protected ComponentHelper.Transformer transformer;
+    public int x;
+    public int y;
 
     public TutorialComponent(GuiTutorialBook.Page page) {
         this.page = page;
@@ -19,13 +21,19 @@ public abstract class TutorialComponent implements IRenderable {
         return false;
     }
 
-    public void onClick() {
+    public void onClick(int mouseX, int mouseY, int key) {
     }
 
-    public void onHover() {
+    public void onHover(int mouseX, int mouseY) {
+    }
+
+    public boolean isHovered(int mouseX, int mouseY) {
+        return false;
     }
 
     public void fromJson(JsonObject object) {
         transformer = new ComponentHelper.Transformer(object.getAsJsonObject("transform"));
+        x = object.getAsJsonPrimitive("x").getAsInt();
+        y = object.getAsJsonPrimitive("y").getAsInt();
     }
 }
