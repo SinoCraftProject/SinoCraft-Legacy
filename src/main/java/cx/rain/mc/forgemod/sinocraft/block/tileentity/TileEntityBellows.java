5915eb82aa8e6fbe87b148530991b0ae0812939f
theflysong <song_of_the_fly@163.com>
2021-07-04 22:16:26 +0800

    update

---
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/block/tileentity/TileEntityBellows.java
@@ -0,0 +1,65 @@
+package cx.rain.mc.forgemod.sinocraft.block.tileentity;
+
+import cx.rain.mc.forgemod.sinocraft.api.capability.IWindEnergy;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static cx.rain.mc.forgemod.sinocraft.api.capability.CapabilityWindEnergy.CAPABILITY;
+
+public class TileEntityBellows extends TileEntity implements ITickableTileEntity {
+    private int we = 0;
+    private int cd = 0;
+
+    public TileEntityBellows() {
+        super(ModTileEntities.BELLOWS.get());
+    }
+
+    public void addWe() {
+        we ++;
+    }
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CAPABILITY) {
+            return LazyOptional.of(() -> new IWindEnergy() {
+                @Override
+                public int getWindEnergy() {
+                    return we;
+                }
+
+                @Override
+                public void setWindEnergy(int we) {
+                }
+
+                @Override
+                public void resetWindEnergy() {
+                }
+
+                @Override
+                public void addWindEnergy(int we) {
+                }
+
+                @Override
+                public void subWindEnergy(int we) {
+                }
+            }).cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void tick() {
+        if (cd >= 0)
+            cd --;
+        if (we >= 0 && cd <= 0) {
+            we --;
+            cd = 200;
+        }
+    }
+}
