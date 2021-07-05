cdbf3a1639183d4b7f2af2af695462e8d2c40656
luqin2007 <lq2007lq@hotmail.com>
2021-07-05 23:56:47 +0800

    add: nonnull

---
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/group/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package cx.rain.mc.forgemod.sinocraft.group;
+
+// todo lq2007
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
