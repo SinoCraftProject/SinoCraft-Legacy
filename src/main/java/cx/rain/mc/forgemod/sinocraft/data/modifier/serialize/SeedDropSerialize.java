64241403fd18e2bcb653f0fd54d78ab578ec67ad
theflysong <song_of_the_fly@163.com>
2021-07-15 14:41:17 +0800

    update seed_drop

--- src/main/java/cx/rain/mc/forgemod/sinocraft/data/modifier/serialize/SeedDropSerialize.java
+++ src/main/java/cx/rain/mc/forgemod/sinocraft/data/modifier/serialize/SeedDropSerialize.java
@@ -2,28 +2,23 @@ package cx.rain.mc.forgemod.sinocraft.data.modifier.serialize;
 
 import com.google.gson.JsonObject;
 import cx.rain.mc.forgemod.sinocraft.data.modifier.GrassDropModifier;
+import cx.rain.mc.forgemod.sinocraft.utility.CraftingHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class SeedDropSerialize extends GlobalLootModifierSerializer<GrassDropModifier> {
     @Override
     public GrassDropModifier read(ResourceLocation location, JsonObject object, ILootCondition[] ailootcondition) {
-        ItemStack stack = new ItemStack(
-                ForgeRegistries.ITEMS.getValue(new ResourceLocation(object.getAsJsonObject("item").getAsJsonPrimitive("id").getAsString())),
-                object.getAsJsonObject("item").getAsJsonPrimitive("count").getAsInt());
+        ItemStack stack = CraftingHelper.deserializeItem(object.get("item"));
         return new GrassDropModifier(ailootcondition, stack);
     }
 
     @Override
     public JsonObject write(GrassDropModifier instance) {
         JsonObject object = makeConditions(instance.getConditions());
-        JsonObject stack = new JsonObject();
-        stack.addProperty("id", instance.stack.getItem().getRegistryName().toString());
-        stack.addProperty("count", instance.stack.getCount());
-        object.add("item", stack);
+        object.add("item", CraftingHelper.serializeItem(instance.stack));
         return object;
     }
 }
